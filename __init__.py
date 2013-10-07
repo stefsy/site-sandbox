8278c2669ca79d37db5a010bdd51de74493f97f1
Stef Sy <steffisy@gmail.com>
2013-10-07 18:10:12 +0800

    deploy pain agony confusion

---
+++ __init__.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, session, g, redirect, url_for, render_template
+
+#configure
+#DEBUG = True #DO NOT USE THIS ON PRODUCTION
+SECRET_KEY = 'development key'
+
+#actual app creation
+app = Flask(__name__)
+
+# loading the configuration from the all caps chunk above
+# usually it's a better idea to load it from a file using from_envvar()
+app.config.from_object(__name__) 
+
+#static empty pages
+@app.route("/")
+def homepage():
+	return render_template('homepage.html')
+
+#route generates nice URLS
+@app.route("/rects/")
+def rects():
+ 	return render_template('rects.html')
+
+@app.route("/nodes/")
+def nodes():
+ 	return render_template('nodes.html')
+
+@app.route("/sort_bars/")
+def sort_bars():
+ 	return render_template('sort_bars.html')
+
+@app.route("/life/")
+def womp():
+	return "It doesn't make much sense."
+
+	#run the app
+if __name__ == "__main__":
+	app.run()
+
+
